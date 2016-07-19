02398535007addd396ceda1a997e97d9fcb6612d
Gabriel Desjardins <gdesjardin@teksystems.com>
2016-07-19 16:55:56 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/app.ts',
+  output: {
+    path: './dist',
+    filename: 'app.[hash].js'
+  },
+
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+        baseHref: '/',
+        template: './src/index.ejs',
+        inject: 'body'
+      })
+  ],
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts' }
+    ]
+  }
+}
